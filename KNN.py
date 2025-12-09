9d14a6c951ab74d9765ba00f99565306efc654a7
Mennahassann <mennahassaneissa@gmail.com>
2025-12-09 17:13:42 +0200

    fixx

--- KNN.py
+++ KNN.py
@@ -2,28 +2,21 @@ import numpy as np
 from sklearn.preprocessing import StandardScaler
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import train_test_split
-import joblib
 
 
-# Load features
-X = np.load('X_mobilenet.npy')
+X = np.load('x_features.npy')
 y = np.load('y_labels.npy')
 
-# Scale features
 scaler = StandardScaler()
 X_scaled = scaler.fit_transform(X)
 
-# Save scaler
-joblib.dump(scaler, "scaler.pkl")
 
 k_values = [3, 5, 7]
 
-# Split data
 X_train, X_test, y_train, y_test = train_test_split(
     X_scaled, y, test_size=0.2, random_state=42, stratify=y, shuffle=True
 )
 
-# Train and evaluate
 for k in k_values:
     knn = KNeighborsClassifier(n_neighbors=k, weights='uniform', metric='euclidean')
     knn.fit(X_train, y_train)
